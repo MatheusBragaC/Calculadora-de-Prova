98984934dd5741cb231fbf7ed70f0724519ce18b
Matheus Braga de Carvalho <159183306+MatheusBragaC@users.noreply.github.com>
2024-07-25 13:15:46 -0300

    Add files via upload

--- index.js
+++ index.js
@@ -1,21 +1,19 @@
 function registrarAluno(e) {
     e.preventDefault();
     const nome = document.getElementById('nome').value;
-    const matricula = document.getElementById('matricula').value;
     const notaum = parseFloat(document.getElementById('notaum').value);
     const notadois = parseFloat(document.getElementById('notadois').value);
     const media = (notaum + notadois) / 2;
     const decisao = media >= 5 ? "Aprovado" : "Reprovado";
-    const linha = criarLinhaTabela(nome, matricula, notaum, notadois, media, decisao);
+    const linha = criarLinhaTabela(nome, notaum, notadois, media, decisao);
     adicionarLinhaTabela(linha);
     limparCamposFormulario();
 }
 
-function criarLinhaTabela(nome, matricula, notaum, notadois, media, decisao) {
+function criarLinhaTabela(nome, notaum, notadois, media, decisao) {
     const linha = document.createElement('tr');
     linha.innerHTML = `
         <td>${nome}</td>
-        <td>${matricula}</td>
         <td>${notaum}</td>
         <td>${notadois}</td>
         <td>${media.toFixed(2)}</td>
@@ -31,7 +29,6 @@ function adicionarLinhaTabela(linha) {
 
 function limparCamposFormulario() {
     document.getElementById('nome').value = '';
-    document.getElementById('matricula').value = '';
     document.getElementById('notaum').value = '';
     document.getElementById('notadois').value = '';
 }
